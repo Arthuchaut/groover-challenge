a6abe36d3570d3d190526b2d23f5a8503edaf87e
Arthuchaut <arthuchaut@gmail.com>
2021-08-10 18:47:16 +0200

    Added Config class and added Spotify API scope in

---
+++ core/config.py
@@ -0,0 +1,28 @@
+import os
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    '''
+    Configuration class for core.
+
+    Attributes:
+        SPOTIFY_API_REDIRECT_URI (str): The Spotify API redirect URI.
+        SPOTIFY_API_SCOPE (list[str]): The Spotify API scope.
+        SPOTIFY_API_CLIENT_ID (str): The Spotify API client ID.
+        SPOTIFY_API_CLIENT_SECRET (str): The Spotify API client secret.
+        POSTGRES_HOST (str): The PostgreSQL host.
+        POSTGRES_PORT (int): The PostgreSQL port.
+        POSTGRES_USER (str): The PostgreSQL user.
+        POSTGRES_PASS (str): The PostgreSQL password.
+    '''
+
+    SPOTIFY_API_REDIRECT_URI: str = os.environ['SPOTIFY_API_REDIRECT_URI']
+    SPOTIFY_API_SCOPE: list[str] = os.environ['SPOTIFY_API_SCOPE'].split()
+    SPOTIFY_API_CLIENT_ID: str = os.environ['SPOTIFY_API_CLIENT_ID']
+    SPOTIFY_API_CLIENT_SECRET: str = os.environ['SPOTIFY_API_CLIENT_SECRET']
+    POSTGRES_HOST: str = os.environ['POSTGRES_HOST']
+    POSTGRES_PORT: int = int(os.environ['POSTGRES_PORT'])
+    POSTGRES_USER: str = os.environ['POSTGRES_USER']
+    POSTGRES_PASS: str = os.environ['POSTGRES_PASS']
