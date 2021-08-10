0126a16dfe0d4d5e45f90b0900f6e846ee0a9808
Arthuchaut <arthuchaut@gmail.com>
2021-08-10 20:43:44 +0200

    Added api views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import View
+from django.http import HttpRequest, HttpResponse
+from api.libs.spotify.spotify_manager import SpotifyManager
+from django.conf import settings
 
-# Create your views here.
+
+class ArtistView(View):
+    '''
+    The /api/artists/ view class.
+    '''
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        '''
+        The GET method implementation.
+
+        Args:
+            request (HttpRequest): The HTTP request object.
+
+        Returns:
+            HttpResponse: A response containing the JSON serialized
+                artist list.
+        '''
+
+        if not request.user.is_authenticated:
+            return redirect('user:auth')
+
+        return HttpResponse('Artists view not implemented yet.')
