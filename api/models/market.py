2a2c8e52f7a29bba3666ecacedf3cd37fa00ce32
Arthuchaut <arthuchaut@gmail.com>
2021-08-13 00:06:13 +0200

    [Cleaning] Corrected country_code field name in Market model

--- api/models/market.py
+++ api/models/market.py
@@ -14,7 +14,7 @@ class Market(models.Model):
     market_id: models.BigAutoField = models.BigAutoField(
         auto_created=True, primary_key=True, serialize=False
     )
-    coutry_code: models.CharField = models.CharField(max_length=2)
+    country_code: models.CharField = models.CharField(max_length=2)
 
     class Meta:
         app_label: str = 'api'
